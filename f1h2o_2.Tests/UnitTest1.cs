5110054784d5a0e1c5248a8876401fa7e86d954a
thokata <thokata@16402-BIF71H8I1>
2016-11-21 17:38:08 -0600

    Add project files.

---
+++ f1h2o_2.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace f1h2o_2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ f1h2o_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="f1h2o_2.MvcApplication" Language="C#" %>
